514a5681fe8a5a67370d28fec6c81208a783e5a9
SidathDabare <sidath2007@gmail.com>
2022-04-06 22:48:06 +0200

    corection

--- Homework02.js
+++ Homework02.js
@@ -7,18 +7,19 @@ ASSIGNMENT RULES
 - You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
 - The solution must be available for the tutors by the end of the day (5PM CET)
 */
-console.log("\n------------EXERCISE 1------------\n");
+
 /* EXERCISE 1
  Create a variable and assign to it an array containing the first 5 positive numbers.
 */
-
+console.log("\n------------EXERCISE 1------------\n");
 /* WRITE YOUR ANSWER HERE */
 let num = [1, 3, 6, 8, 9];
-console.log("\n------------EXERCISE 2------------\n");
+console.log(num);
+
 /* EXERCISE 2
  Create a variable and assign to it an object containing your name, surname, email address and age.
 */
-
+console.log("\n------------EXERCISE 2------------\n");
 /* WRITE YOUR ANSWER HERE */
 let person = {
   name: "Sidath",
@@ -26,27 +27,27 @@ let person = {
   email: "example1@email.com",
   age: 35,
 };
-console.log("\n------------EXERCISE 3------------\n");
+console.log(person);
 /* EXERCISE 3
  Add to the previously created object a property with a boolean value to rappresent wheter you have or not a driving license.
 */
-
+console.log("\n------------EXERCISE 3------------\n");
 /* WRITE YOUR ANSWER HERE */
 person.hasDrivingLicense = true;
 console.log(person);
-console.log("\n------------EXERCISE 4------------\n");
+
 /* EXERCISE 4
  Remove from the previously created object the age property.
 */
-
+console.log("\n------------EXERCISE 4------------\n");
 /* WRITE YOUR ANSWER HERE */
 delete person.age;
 console.log(person);
-console.log("\n------------EXERCISE 5------------\n");
+
 /* EXERCISE 5
  Create a second object with another name, surname, email address and verify that this object has a different email address than the previous one.
 */
-
+console.log("\n------------EXERCISE 5------------\n");
 /* WRITE YOUR ANSWER HERE */
 let person1 = {
   name: "Jhon",
@@ -59,13 +60,13 @@ if (person.email == person1.email) {
 } else {
   console.log("Different email");
 }
-console.log("\n------------EXERCISE 6------------\n");
+
 /* EXERCISE 6
  You are working on an e-commerce website. In the variable totalShoppingCart you are storing the total amount spent by the current user.
  Currently you have a promotion: if the customer's shopping cart total is more than 50, the user is eligible for free shipping (otherwise it costs 10).
  Write an algorithm that calculates the total cost to charge the user with.
 */
-
+console.log("\n------------EXERCISE 6------------\n");
 /* WRITE YOUR ANSWER HERE */
 let totalShoppingCart = 50;
 let amountSpent = 41;
@@ -75,12 +76,11 @@ if (amountSpent > totalShoppingCart) {
   console.log("Shipping cost $10");
 }
 
-console.log("\n------------EXERCISE 7------------\n");
 /* EXERCISE 7
  You are working on an e-commerce website. Today is Black Friday and everything has a 20% discount at the end of the purchase.
  Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
 */
-
+console.log("\n------------EXERCISE 7------------\n");
 /* WRITE YOUR ANSWER HERE */
 let discount = (amountSpent / 100) * 20;
 let newTotal = discount + amountSpent;
@@ -90,49 +90,92 @@ if (amountSpent > totalShoppingCart) {
   console.log("Your total $: " + newTotal + "+ Shipping cost $10");
 }
 console.log(newTotal);
-console.log("\n------------EXERCISE 8------------\n");
+
 /* EXERCISE 8
  Create a variable and assign to it an object representing a car, with properties like brand, model and licensePlate.
  Then clone it 5 times, and change the licensePlate for each cloned car without affecting the original one.
 */
-
+console.log("\n------------EXERCISE 8------------\n");
 /* WRITE YOUR ANSWER HERE */
-let car = {
+let car1 = {
   brand: "BMW",
   model: "5siries",
   licensePlate: "DD6666GG",
 };
+let car2 = {
+  brand: "BMW",
+  model: "5siries",
+  licensePlate: "DD6666GG",
+};
+let car3 = {
+  brand: "BMW",
+  model: "5siries",
+  licensePlate: "DD6666GG",
+};
+let car4 = {
+  brand: "BMW",
+  model: "5siries",
+  licensePlate: "DD6666GG",
+};
+let car5 = {
+  brand: "BMW",
+  model: "5siries",
+  licensePlate: "DD6666GG",
+};
+//let newLicensePlate = Object.assign((car2 = { licensePlate: "DD6666Ga" }));
+car2.licensePlate = "DD6666Ga";
+car3.licensePlate = "DD6666Gb";
+car4.licensePlate = "DD6666Gc";
+car5.licensePlate = "DD6666Gd";
+// console.log(newLicensePlate);
+console.log(car5);
 
 /* EXERCISE 9
  Create a variable called carsForRent and assign to it an array containing all the cars from the previous exercise.
+ let 
 */
-
+console.log("\n------------EXERCISE 9------------\n");
 /* WRITE YOUR ANSWER HERE */
+//let carsForRent = [car1, car2, car3, car4, car5];
+// console.log(carsForRent);
 
 /* EXERCISE 10
  Remove the first and the last car from the carsForRent array.
 */
-
+console.log("\n------------EXERCISE 10------------\n");
 /* WRITE YOUR ANSWER HERE */
+let carsForRent = [car1, car2, car3, car4, car5];
+carsForRent.shift(0, 1);
+carsForRent.splice(3, 1);
+// carsForRent.shift(0, 1);
+//delete carsForRent[0];
+console.log(carsForRent);
 
 /* EXERCISE 11
  Print to the console the type of the car variable you created before, as well as the types of its licensePlate and brand properties.
 */
-
+console.log("\n------------EXERCISE 11------------\n");
 /* WRITE YOUR ANSWER HERE */
 
 /* EXERCISE 12
  Create a new variable called carsForSale assigning to it an empty array, and then insert 3 cars into it.
  Create a new variable called totalCars and assign to it the total number of cars present in the carsForSale and carsForRent arrays.
 */
-
+console.log("\n------------EXERCISE 12------------\n");
 /* WRITE YOUR ANSWER HERE */
+let carsForSale = [];
+carsForSale = carsForRent;
+let totalCars = carsForSale;
+console.log(totalCars);
 
 /* EXERCISE 13
  Using a loop, print to the console all the data for each car in the carsForSale array.
 */
-
+console.log("\n------------EXERCISE 14------------\n");
 /* WRITE YOUR ANSWER HERE */
+for (let i = 0; i < totalCars.length; i++) {
+  console.log(totalCars[i]);
+}
 
 /* WHEN YOU ARE FINISHED
  Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
