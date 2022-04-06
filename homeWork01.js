32fb7a81e841a13133a461e488f45d480708940c
SidathDabare <sidath2007@gmail.com>
2022-04-06 16:59:24 +0200

    correction

---
+++ homeWork01.js
@@ -0,0 +1,78 @@
+// Additional assignments for Day 4
+
+/* EXTRA 1
+ Write a piece of code for reverting an array.
+ es:
+ [1, 3, 5] ==> [5, 3, 1]
+*/
+
+/* WRITE YOUR ANSWER HERE */
+// let numbers = [1, 3, 5];
+// console.log("firstLine:", numbers);
+
+/* EXTRA 2
+ Write a piece of code for getting the maximum numerical value from an array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+// const array1 = [1, 3, 2, 7, 9, 23, 5];
+
+// console.log(Math.max(...array1));
+
+/* EXTRA 3
+ Write a piece of code for getting the minimum numerical value from an array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+// console.log(Math.min(...array1));
+
+/* EXTRA 4
+ Write a piece of code for getting only even numerical values from an array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+let array2 = [1, 3, 2, 7, 9, 23, 5];
+for (let index = 0; index < students.length; index++) {
+  students[index].age += 1;
+}
+
+console.log(students);
+
+/* EXTRA 5
+ Write a piece of code for deleting only even entries from an array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXTRA 6
+ Write a piece of code for removing all the vowels from a string.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+// const str = "The quick brown fox jumps over a lazy dog";
+// const vowels = str.match(/[aeiou]/gi);
+// const consonants = str.match(/[^aeiou]/gi);
+// vowels.concat([""], consonants).forEach((k) => {
+//   console.log(k);
+// });
+
+/* EXTRA 7
+ Write a piece of code for increasing all the numerical values in a array by 1.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+// let array3 = [1, 3, 2, 7, 9, 23, 5];
+// for (let index = 0; index < array3.length; index++) {
+//   array3[index] += 1;
+// }
+// console.log(array3);
+/* EXTRA 8 
+ Replace all the strings contained in an array with their length.
+ es.: ["strive", "is", "great"] => [6, 2, 5]
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* WHEN YOU ARE FINISHED
+ Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
