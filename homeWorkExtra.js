314c5551dd56d426db902c2c4944e3d80084ba35
SidathDabare <sidath2007@gmail.com>
2022-04-06 23:54:08 +0200

    Correction

--- homeWorkExtra.js
+++ homeWorkExtra.js
@@ -5,15 +5,16 @@
  es:
  [1, 3, 5] ==> [5, 3, 1]
 */
+console.log("\n------------EXTRA 1------------\n");
 
 /* WRITE YOUR ANSWER HERE */
-// let numbers = [1, 3, 5];
-// console.log("firstLine:", numbers);
+let numbers = [1, 3, 5];
+console.log(numbers.reverse());
 
 /* EXTRA 2
  Write a piece of code for getting the maximum numerical value from an array.
 */
-
+console.log("\n------------EXTRA 2------------\n");
 /* WRITE YOUR ANSWER HERE */
 const array1 = [1, 3, 2, 7, 9, 23, 5];
 let max = 0;
@@ -21,39 +22,67 @@ for (let i = 0; i < array1.length; i++) {
   if (array1[i] > max) {
     max = array1[i];
   }
-  console.log(array1[i], "is samaller than", max);
+  //console.log(array1[i]);
+  //console.log(array1[i], "is samaller than", max);
 }
-console.log(max);
+console.log("maximum number is ", max);
 
 /* EXTRA 3
  Write a piece of code for getting the minimum numerical value from an array.
 */
-
+console.log("\n------------EXTRA 3------------\n");
 /* WRITE YOUR ANSWER HERE */
+const array2 = [8, 3, 2, 7, 9, 23, 5];
+let min = array2[0];
+for (let i = 1; i < array2.length; i++) {
+  if (array2[i] < min) {
+    min = array1[i];
+  }
+  //console.log(array2[i]);
+  //console.log(array1[i], "is greter than", min);
+}
+console.log("minimum number is ", min);
+
 // console.log(Math.min(...array1));
 
 /* EXTRA 4
  Write a piece of code for getting only even numerical values from an array.
 */
+console.log("\n------------EXTRA 4------------\n");
 
 /* WRITE YOUR ANSWER HERE */
-// let array2 = [1, 3, 2, 7, 9, 23, 5];
-// for (let index = 0; index < students.length; index++) {
-//   students[index].age += 1;
-// }
+const array3 = [8, 3, 2, 4, 9, 23, 5];
+for (let i = 0; i < array3.length; i++) {
+  if (array3[i] % 2 == 1) {
+    console.log(array3[i]);
+  }
+}
 
 // console.log(students);
 
 /* EXTRA 5
  Write a piece of code for deleting only even entries from an array.
 */
+console.log("\n------------EXTRA 5------------\n");
 
 /* WRITE YOUR ANSWER HERE */
 
+const array4 = [8, 3, 2, 4, 9, 23, 5, 11, 17, 7];
+
+for (let i = 0; i < array4.length; i++) {
+  if (array4[i] % 2 == 1) {
+    delete array4[i];
+
+    //array4.splice(evan);
+    //console.log(array4.splice(array4[i], 1));
+    //console.log(delete array4[i]);
+  }
+}
+console.log(array4);
 /* EXTRA 6
  Write a piece of code for removing all the vowels from a string.
 */
-
+console.log("\n------------EXTRA 6------------\n");
 /* WRITE YOUR ANSWER HERE */
 // const str = "The quick brown fox jumps over a lazy dog";
 // const vowels = str.match(/[aeiou]/gi);
@@ -65,18 +94,20 @@ console.log(max);
 /* EXTRA 7
  Write a piece of code for increasing all the numerical values in a array by 1.
 */
+console.log("\n------------EXTRA 7------------\n");
 
 /* WRITE YOUR ANSWER HERE */
-// let array3 = [1, 3, 2, 7, 9, 23, 5];
-// for (let index = 0; index < array3.length; index++) {
-//   array3[index] += 1;
-// }
-// console.log(array3);
+let array5 = [1, 3, 2, 7, 9, 23, 5];
+for (let index = 0; index < array5.length; index++) {
+  array5[index] += 1;
+}
+console.log(array5);
+
 /* EXTRA 8 
  Replace all the strings contained in an array with their length.
  es.: ["strive", "is", "great"] => [6, 2, 5]
 */
-
+console.log("\n------------EXTRA 8------------\n");
 /* WRITE YOUR ANSWER HERE */
 
 /* WHEN YOU ARE FINISHED
